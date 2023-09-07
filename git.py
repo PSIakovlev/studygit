d4fbfb3eb00d36ea7d9d8923f26175dfc50d0963
Pavel <ikpas21041997@mail.ru>
2023-09-08 02:27:08 +0500

    первый commit

---
+++ git.py
@@ -0,0 +1,13 @@
+# https://git-scm.com/
+
+# в командной строке
+# git --version
+# git --help
+# git init
+# git status
+# git add <имя файла>
+# git rm --cached <имя файла>
+# git commit
+# git commit -m "первый commit"
+# добавил изменения
+
